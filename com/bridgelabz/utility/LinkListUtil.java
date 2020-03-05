160944c31f5defbb39cf79349a54c5a25b4c4a39
kishlayswapnil <kishlayswapnil@gmail.com>
2020-03-05 16:54:43 +0530

    Added: Add method has been added.

--- com/bridgelabz/utility/LinkListUtil.java
+++ com/bridgelabz/utility/LinkListUtil.java
@@ -2,10 +2,10 @@ package com.bridgelabz.utility;
 
 //A generic link list utility class.
 public class LinkListUtil<T> {
-
+	
 	//Head for the linked list been initialized.
 	Node head;
-
+	
 	//List has been initialized.
 	public String list() {
 		String status;
@@ -19,4 +19,22 @@ public class LinkListUtil<T> {
 		}
 		return status;
 	}
+	
+	//Add method has been initialized to enter user data at the next value.
+	public void add(T item) {
+		Node newNode = new Node();
+		newNode.data = item;
+		newNode.next = null;
+		if (list() != null) {
+			head = newNode;
+		} else {
+			Node node1 = head;
+			//Condition to traverse through the linked list and add the data at the end.
+			while (node1.next != null) {
+				node1 = node1.next;
+			}
+			node1.next = newNode;
+		}
+	}
+	
 }
