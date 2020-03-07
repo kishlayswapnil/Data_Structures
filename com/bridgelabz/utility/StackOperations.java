bcbfa14f446ef3403eba816ea13df7f83145370c
kishlayswapnil <kishlayswapnil@gmail.com>
2020-03-07 11:23:29 +0530

    Added: Push an increase limit methods have been added.

---
+++ com/bridgelabz/utility/StackOperations.java
@@ -0,0 +1,26 @@
+package com.bridgelabz.utility;
+
+//Stack operation class in a utility package.
+public class StackOperations<T> {
+	int capacity = 10;
+	private int top = 0;
+	private Object[] stack = new Object[capacity];
+	
+	public void push(T data) {
+		// checks for capacity of array
+		if (top == capacity){
+			increaseLimit();
+		}
+		//Puts the data into the stack
+		stack[top] = data;
+		top++;
+	}
+	
+	//Function to increase the limit of the stack if the capacity has reached.
+	private void increaseLimit() {
+		Object[] newStack = new Object[capacity * 2];
+		System.arraycopy(stack, 0, newStack, 0, top);
+		capacity *= 2;
+		stack = newStack;
+	}
+}
